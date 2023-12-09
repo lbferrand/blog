1603e2591c50f4cb1c1362cd65aed0bb28408b5e
Luciana B Ferrand <lbferrand@gmail.com>
2023-12-08 21:08:21 -0500

    Create a new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,33 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find_by(id: params[:id])
+    @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  # the create action is responsible for creating a new article record in the database 
+  # with the values for the title and body columns taken from the params hash. 
+  # If the article saves successfully, the user is redirected to the article's show page. 
+  # If the article fails to save, the new template is rendered again with the @article variable passed in.
+  
+  def create
+    @article = Article.new(article_params) 
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # Add private method to the bottom of the controller file. 
+  # This method will avoid for malicious users could potentially submit extra form fields and 
+  # overwrite existing values in the database.
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
+# this model will add validation to the article model
+# The first validation ensures that the title is present,
+# and the second validation ensures that the body is present and at least 10 characters long.
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
