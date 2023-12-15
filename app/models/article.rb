7d98063db5e42bc804825a77717b0f63852fe1cb
Luciana B Ferrand <lbferrand@gmail.com>
2023-12-14 19:34:38 -0500

    Add rails action_text (rich text)

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,7 @@ class Article < ApplicationRecord
   include Visible
 
   has_many :comments, dependent: :destroy
+  has_rich_text :content
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
